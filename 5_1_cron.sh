19b8426b8a960d2757b5e427775a46fc18f04771
saeb <saeb.m.13@gmail.com>
2021-09-11 09:45:14 +0430

    init

---
+++ 5_1_cron.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+red="$(tput setaf 1)"
+green="$(tput setaf 2)"
+orange="$(tput setaf 3)"
+white="$(tput sgr0)"
+
+# ---------------
+
+hardening() {
+    echo ------------------------------
+    echo
+    echo "${orange}$4${white}"
+    echo
+
+    echo "${orange}running:${white} $2"
+
+    if eval "$2 | grep -i $3"; then
+        echo
+        echo "${green}PASSED!${white}"
+    else
+        echo "${orange}hardening:${white} $1"
+        if eval "$1"; then
+            if eval "$2 | grep -i $3"; then
+                echo
+                echo "${green}PASSED!${white}"
+            else
+                echo
+                echo "${red}FAILED!${white}"
+                echo
+                exit 1
+            fi
+        else
+            echo
+            echo "${red}FAILED!${white}"
+            echo
+            exit 1
+        fi
+    fi
+    echo
+}
+
+clear
+
+hardening 'systemctl --now enable cron' 'systemctl is-enabled cron' 'enabled' '5.1.1 Ensure cron daemon is enabled (Scored)'
+hardening 'chown root:root /etc/crontab' 'stat /etc/crontab' 'Uid: (0/root)Gid: (0/root)' '5.1.2 Ensure permissions on /etc/crontab are configured (Scored)'
+hardening 'chmod og-rwx /etc/crontab' 'stat /etc/crontab' 'Access: (0600/-rw-------)' '5.1.2 Ensure permissions on /etc/crontab are configured (Scored)'
